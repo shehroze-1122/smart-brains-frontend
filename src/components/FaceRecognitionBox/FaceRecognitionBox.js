b5eafde6fdf9ed70c1ab5b0998737c6a501477d3
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2021-09-09 15:29:31 +0500

    refactored whole app

--- src/components/FaceRecognitionBox/FaceRecognitionBox.js
+++ src/components/FaceRecognitionBox/FaceRecognitionBox.js
@@ -11,23 +11,23 @@ const FaceRecognitionBox = () => {
             return(
                 <div className="image center ma3 pa3" >
                     <div className="absolute mt2">
-                    <Tilt options={{
-                    reverse: false,
-                    max: 10,
-                    speed: 300,
-                    transition: true,
-                    axis: null,
-                    reset: true,
-                    easing: 'cubic-bezier(.03,.98,.52,.99)'}} >
+                        <Tilt options={{
+                        reverse: false,
+                        max: 10,
+                        speed: 300,
+                        transition: true,
+                        axis: null,
+                        reset: true,
+                        easing: 'cubic-bezier(.03,.98,.52,.99)'}} >
 
-                        <img id="img" src={imageUrl} width="600px"  alt='' />
-                    
-                        {   
-                            boxValues.map((faceBoxCoordinates, ind)=>{
-                                return (<div className="bounding-box" key={ind} style={{'top':faceBoxCoordinates.top_row,  'left':faceBoxCoordinates.left_col, 'right':faceBoxCoordinates.right_col, 'bottom':faceBoxCoordinates.bottom_row}}></div>)
+                            <img id="img" src={imageUrl} width="600px"  alt='' />
+                        
+                            {   
+                                boxValues.map((faceBoxCoordinates, ind)=>{
+                                    return (<div className="bounding-box" key={ind} style={{'top':faceBoxCoordinates.top_row,  'left':faceBoxCoordinates.left_col, 'right':faceBoxCoordinates.right_col, 'bottom':faceBoxCoordinates.bottom_row}}></div>)
                                 })
-                        }
-                    </Tilt>
+                            }
+                        </Tilt>
                     </div>
                 </div> 
             ) 
--- src/containers/App.js
+++ src/containers/App.js
@@ -31,12 +31,7 @@ const params = {
   }
 }
 
-
-
-
 const App = ()=> {
-
-
 
   return (
     <div>
